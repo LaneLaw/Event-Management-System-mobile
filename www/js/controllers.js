20caade5c7a08e945d0c730033d6e32e33909547
LaneLu <1537167855@qq.com>
2018-12-01 14:45:00 +0800

    Try to load sails event

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,13 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('homeCtrl', ['$scope', '$stateParams','$http', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
-
+function ($scope, $stateParams, $http) {
+    $http.get("https://api.myjson.com/bins/fgrmm")
+        .then(function(response){
+          $scope.feeds = response.data;
+      });
 
 }])
    
--- www/js/services.js
+++ www/js/services.js
@@ -4,6 +4,29 @@ angular.module('app.services', [])
 
 }])
 
-.service('BlankService', [function(){
-
+.service('Event', [function(){
+    var Venues = [
+        {
+          "VenueID": "POD",
+          "VenueName": "Podium, Level 6",
+          "Latitude": 22.340778,
+          "Longitude": 114.179943,
+          "CampusID": "HSH",
+          "id": 1,
+        },
+        {
+          "VenueID": "SWT501",
+          "VenueName": "Shaw Tower 501",
+          "Latitude": 22.338867,
+          "Longitude": 114.181909,
+          "CampusID": "SHAW",
+          "id": 2,
+        },
+    ];
+      this.getAllVenues = function(){
+          return Venues;
+      }
+      this.getVenueByID = function(id){
+        return Venues[id - 1];
+    }
 }]);
